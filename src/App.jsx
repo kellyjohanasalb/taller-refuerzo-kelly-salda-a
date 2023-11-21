b4d174b43e988df2e1352a6395a5e0b774f084da
kelly saldaña <kellysalb@hotmail.com>
2023-11-21 16:02:14 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    
+        <p>
+          Edita el archivo <code>src/App.js</code> y guarda para recargar.
+        </p>
+        <a
+          className="App-link"
+          href="https://platzi.com/reactjs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
